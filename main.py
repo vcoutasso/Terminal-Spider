86046e8cf0ce0a018a80885b9960f399e732ab45
Vinícius Couto <vinicouto12@gmail.com>
2019-03-03 23:36:26 -0300

    commit pq to com sono

--- main.py
+++ main.py
@@ -7,14 +7,21 @@ random.shuffle(deck)
 random.shuffle(deck)
 random.shuffle(deck)
 
-#print(deck)
+def print_table():
+    print("┌────┐\t\t┌────┐\t\t┌────┐\t\t┌────┐\n|    |\t\t|    |\t\t|    |\t\t|    |\n")
 
 spades = "\u2660"
 
-card = "┌────┐ \n|7  " + spades + "|\n|    |\n|    |\n└────┘"
+card = "┌────┐ \n|10 " + spades + "|\n|    |\n|    |\n└────┘"
 
 print(card)
 
 card_back = "┌────┐\n| /\ |\n| -- |\n| \/ |\n└────┘"
 
 print(card_back)
+
+deck_cards = "┌────┐┐\n| /\ ||\n| -- ||\n| \/ ||\n└────┘┘"
+
+print(deck_cards)
+print("\n\n")
+print_table()
